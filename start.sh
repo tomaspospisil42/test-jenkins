f4e52421e2e4ce2add295548d73ce26e1ba8328a
Tomas Pospisil <tomas.pospisil@solarwinds.com>
2022-11-04 15:50:17 +0100

    jenkins with docker in docker support

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+$jenkins_data_folder=/Users/Tomas.Pospisil/src/jenkins_data
+
+docker build --tag jenkins-in-docker .
+
+docker run --name jenkins-with-docker -d -p 8080:8080 -p 50000:50000 --group-add 0 -v ${jenkins_data_folder}:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock jenkins-in-docker
